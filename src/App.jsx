852311a38855fbafafc9f3d3e63bfa1a5afe987f
Dario Junior <106486127+Drario11@users.noreply.github.com>
2026-04-16 09:40:58 -0300

    Atualizando App e Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import Sobre from './Sobre.jsx'
-import Header from './Header.jsx'
-import Contato from './Contato.jsx'
-import Home from './Home.jsx'
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+import Sobre from './Sobre.jsx'
+import Contato from './Contato.jsx'
 
 function App() {
-
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contato" element={<Contato />} />
-      </Routes>
+      <main style={{ padding: '20px' }}>
+        <Routes>
+          {/* Define qual ficheiro abrir em cada link */}
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </main>
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <header style={{ backgroundColor: "#cd5757ff", padding: "20px", borderBottom: "1px solid #ccc" }}>
+        <header style={{ background: '#cd5757', padding: '20px', color: 'white' }}>
             <h2>Bem-vindo ao Meu sistema</h2>
             <nav>
-                <ul style={{ listStyle: "none", display: "flex", gap: "20px" }}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/sobre">Sobre</Link></li>
-                    <li><Link to="/contato">Contato</Link></li>
+                <ul style={{ listStyle: 'none', display: 'flex', gap: '15px' }}>
+                    <li><Link to="/" style={{ color: 'white' }}>Home</Link></li>
+                    <li><Link to="/sobre" style={{ color: 'white' }}>Sobre</Link></li>
+                    <li><Link to="/contato" style={{ color: 'white' }}>Contato</Link></li>
                 </ul>
             </nav>
         </header>

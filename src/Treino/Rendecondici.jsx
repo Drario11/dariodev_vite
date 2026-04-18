132e00d8558649aed10c9ae4fcf0aeea38c756a2
Dario Junior <106486127+Drario11@users.noreply.github.com>
2026-04-18 10:49:58 -0300

    Entendendo Rendirização Condicional

--- src/Treino/Rendecondici.jsx
+++ src/Treino/Rendecondici.jsx
@@ -1,10 +1,42 @@
-function Rendecondici() {
+function Item({ name, isPacked }) {
+    if (isPacked) {
+        return <li className="item">{name} ✅</li>
+    }
+    return <li className="item">{name} ❌</li>
+}
+/*
+    return (
+        <li className="item">
+            {name} {isPacked ? '✅' : '❌'}
+        </li>
+    );
+
+    Essa expressão é chamada de operador ternário. É uma forma curta de escrever uma expressão condicional.
+    Sintaxe: condicao ? valor_se_verdadeiro : valor_se_falso
+    
+*/
+
+export default function PackingList() {
     return (
         <section>
-            <h2>Rendecondici</h2>
-            <p>Este é um projeto para aprender React com Vite</p>
+            <ul>
+                <Item
+                    name="Traja Espacial"
+                    isPacked={true}
+                />
+                <Item
+                    name="Capacete com folha dourada"
+                    isPacked={false}
+                />
+                <Item
+                    name="Foto de tamanho passaporte"
+                    isPacked={true}
+                />
+                <Item
+                    name="Mapa do sistema solar"
+                    isPacked={true}
+                />
+            </ul>
         </section>
-    )
+    );
 }
-
-export default Rendecondici 
